c6f898558b5be221727d8e253c3d050c5ef7fec8
amihoprogrammer <amirhanegareshi@gmail.com>
2025-06-10 17:45:30 +0330

    add props and check type of it

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,20 @@ import Food from "./Food.jsx";
 import Card from "./Card.jsx";
 import Button1 from "./Button/Button1.jsx";
 import Button2 from "./Button2.jsx";
+import Student from "./Student.jsx";
 function App() {
   return (
     <>
       <Header />
-      <Food />
-      <hr />
+      {/*<Food /> <hr />*/}
       <Card />
       <Card />
       <Card />
       <Button1 />
       <Button2 />
+      <Student name="Ali" age={34} isStudent={true} />
+      <Student name="Ali" age="ghi" isStudent={true} />
+      <Student />
       {/**/}
       <Footer />
     </>
---
+++ src/Student.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+function Student(props) {
+  return (
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+      <p>Student: {props.isStudent ? "yes" : "no"}</p>
+    </div>
+  );
+}
+//Debugging
+Student.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  isStudent: PropTypes.bool,
+};
+Student.defaultProps = {
+  name: "mohammed",
+  age: 33,
+  isStudent: true,
+};
+export default Student;
