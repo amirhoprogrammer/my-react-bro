60e889560e500f20e2d28395119588de54f81547
amihoprogrammer <amirhanegareshi@gmail.com>
2025-07-05 10:37:25 +0330

    last edit in home and css

--- src/Home.jsx
+++ src/Home.jsx
@@ -33,68 +33,78 @@ function Home() {
           lesson4
         </Link>
       </div>
-      <p>Lesson5</p>
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/list">
-        lesson5
-      </Link>
-      <hr />
-      <p>Lesson6</p>
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/button2">
-        lesson6 part1
-      </Link>
-      <br />
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/profilepicker">
-        lesson6 part2
-      </Link>
-      <hr />
-      <p>Lesson7</p>
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/mycomponent">
-        lesson7
-      </Link>
-      <hr />
-      <p>Lesson8</p>
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/counter">
-        lesson8
-      </Link>
-      <hr />
-      <p>Lesson9</p>
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/mycomponents">
-        lesson9
-      </Link>
-      <hr />
-      <p>Lesson10</p>
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/colorpicker">
-        lesson10
-      </Link>
-      <hr />
-      <p>Lesson11</p>
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/mycomponentobject">
-        lesson11
-      </Link>
-      <hr />
-      <p>Lesson12</p>
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/mycomponentarray">
-        lesson12
-      </Link>
-      <hr />
-      <p>Lesson13</p>
-      <Link
-        style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }}
-        to="/mycomponentarrayobject"
-      >
-        lesson13
-      </Link>
-      <hr />
-      <p>Lesson14</p>
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/todolist">
-        lesson14
-      </Link>
-      <hr />
-      <p>Lesson15</p>
-      <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="#">
-        lesson15
-      </Link>
-      <hr />
+      <div className="lesson1">
+        <p>Lesson5</p>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/list">
+          lesson5
+        </Link>
+      </div>
+      <div className="lesson1">
+        <p>Lesson6</p>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/button2">
+          lesson6 part1
+        </Link>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/profilepicker">
+          lesson6 part2
+        </Link>
+      </div>
+      <div className="lesson1">
+        <p>Lesson7</p>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/mycomponent">
+          lesson7
+        </Link>
+      </div>
+      <div className="lesson1">
+        <p>Lesson8</p>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/counter">
+          lesson8
+        </Link>
+      </div>
+      <div className="lesson1">
+        <p>Lesson9</p>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/mycomponents">
+          lesson9
+        </Link>
+      </div>
+      <div className="lesson1">
+        <p>Lesson10</p>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/colorpicker">
+          lesson10
+        </Link>
+      </div>
+      <div className="lesson1">
+        <p>Lesson11</p>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/mycomponentobject">
+          lesson11
+        </Link>
+      </div>
+      <div className="lesson1">
+        <p>Lesson12</p>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/mycomponentarray">
+          lesson12
+        </Link>
+      </div>
+      <div className="lesson1">
+        <p>Lesson13</p>
+        <Link
+          style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }}
+          to="/mycomponentarrayobject"
+        >
+          lesson13
+        </Link>
+      </div>
+      <div className="lesson1">
+        <p>Lesson14</p>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="/todolist">
+          lesson14
+        </Link>
+      </div>
+      <div className="lesson1">
+        <p>Lesson15</p>
+        <Link style={{ textDecoration: "none", color: "rgb(3, 193, 120)" }} to="#">
+          lesson15
+        </Link>
+      </div>
       <Footer />
     </>
   );
